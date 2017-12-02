57373547213a96f6df2637ad516e6fb2ca30e455
Keegan <jonesk74@uw.edu>
2017-12-02 12:11:47 -0800

    Updating AABB to be more functional, adding more useful features.

--- include/util/AABB.hpp
+++ include/util/AABB.hpp
@@ -1,25 +1,46 @@
 #pragma once
 #ifndef VULPES_UTIL_AABB_H
 #define VULPES_UTIL_AABB_H
+#include "glm/vec3.hpp"
 
-#include "vpr_stdafx.h"
-
-namespace vulpes {
+namespace vpsk {
 
     namespace util {
 
-        struct AABB {
-
-            glm::vec3 Min, Max;
+        class AABB {
             
-            glm::vec3 Extents() const;
+            AABB() = default;
+            AABB(const glm::vec3& p);
+            AABB(const glm::vec3& p0, const glm::vec3& p1);
+            AABB(const glm::vec3& center, const float& radius);
+            AABB(const AABB& other) noexcept;
+            AABB(AABB&& other) noexcept;
+            AABB& operator=(const AABB& other) noexcept;
+            AABB& operator=(AABB&& other) noexcept;
 
-            glm::vec3 Center() const;
+            static AABB Union(const AABB& initial_bounds, const glm::vec3& point_to_include);
+            static AABB Intersection(const AABB& b0, const AABB& b1);
 
-            void UpdateMinMax(const float& y_min, const float& y_max);
+            glm::vec3 Extents() const noexcept;
+            glm::vec3 Center() const noexcept;
+            bool Overlaps(const AABB& other) const noexcept;
+            bool Contains(const glm::vec3& pt) const noexcept;
+            const glm::vec3& Min() const noexcept;
+            const glm::vec3& Max() const noexcept;
 
+            void Include(const glm::vec3& pt);
+            void Expand(const float& dist);
             bool Intersection(const glm::vec3& origin, const glm::vec3& ray) const;
 
+            glm::vec3 Diagonal() const noexcept;
+            float SurfaceArea() const noexcept;
+            float Volume() const noexcept;
+
+            size_t IdxToLargestAxis() const noexcept;
+            glm::vec3 Lerp(const glm::vec3& amt) const noexcept;
+        private:
+
+            glm::vec3 min, max;
         };
 
         
--- src/util/AABB.cpp
+++ src/util/AABB.cpp
@@ -1,20 +1,77 @@
-#include "vpr_stdafx.h"
 #include "util/AABB.hpp"
-namespace vulpes {
+#include <algorithm>
+#include <cmath>
+
+namespace vpsk {
 
     namespace util {
 
-        glm::vec3 vulpes::util::AABB::Extents() const {
-            return (Min - Max);
+        const float lerp(const float& s, const float& v0, const float& v1) {
+            return std::fma(s, v1, std::fma(-s, v0, v0));
         }
 
-        glm::vec3 vulpes::util::AABB::Center() const {
-            return (Min + Max) / 2.0f;
+        AABB::AABB(const glm::vec3& p) : min(p), max(p) {}
+
+        AABB::AABB(const glm::vec3& p0, const glm::vec3& p1) :
+            min(std::min(p0.x, p1.x), std::min(p0.y, p1.y), std::min(p0.z, p1.z)),
+            max(std::max(p0.x, p1.x), std::max(p0.y, p1.y), std::max(p0.z, p1.z)) {}
+
+        AABB::AABB(const glm::vec3& center, const float& radius) : AABB(center - radius, center + radius) {}
+
+        AABB::AABB(const AABB& other) noexcept : min(other.Min()), max(other.Max()) {}
+
+        AABB::AABB(AABB&& other) noexcept : min(std::move(other.min)), max(std::move(other.max)) {}
+
+        AABB& AABB::operator=(const AABB& other) noexcept {
+            min = other.Min();
+            max = other.Max();
+            return *this;
         }
-        
-        void AABB::UpdateMinMax(const float & y_min, const float & y_max) {
-            Min.y = y_min;
-            Max.y = y_max;
+
+        AABB& AABB::operator=(AABB&& other) noexcept {
+            min = std::move(other.min);
+            max = std::move(other.max);
+            return *this;
+        }
+
+        AABB AABB::Union(const AABB& b, const glm::vec3& p) {
+            return AABB(glm::vec3(std::min(b.Min().x, p.x), std::min(b.Min().y, p.y), std::min(b.Min().z, p.z)),
+                        glm::vec3(std::max(b.Max().x, p.x), std::max(b.Max().y, p.y), std::max(b.Max().z, p.z)));
+        }
+
+        glm::vec3 AABB::Center() const noexcept {
+            return (min + max) / 2.0f;
+        }
+
+        bool AABB::Overlaps(const AABB& b) const noexcept {
+            bool x = (max.x >= b.Min().x) && (min.x <= b.Max().x);
+            bool y = (max.y >= b.Min().y) && (min.y <= b.Max().y);
+            bool z = (max.z >= b.Min().z) && (min.z <= b.Max().z);
+            return (x && y && z);
+        }
+
+        bool AABB::Contains(const glm::vec3& pt) const noexcept {
+            bool x = (pt.x >= min.x) && (pt.x <= max.x);
+            bool y = (pt.y >= min.y) && (pt.y <= max.y);
+            bool z = (pt.z >= min.z) && (pt.z <= max.z);
+            return (x && y && z);
+        }
+
+        const glm::vec3& AABB::Min() const noexcept {
+            return min;
+        }
+
+        const glm::vec3& AABB::Max() const noexcept  {
+            return max;
+        }
+
+        void AABB::Include(const glm::vec3& pt) {
+            *this = std::move(Union(*this, pt));
+        }
+
+        void AABB::Expand(const float& dist) {
+            min -= dist;
+            max += dist;
         }
 
         bool AABB::Intersection(const glm::vec3& origin, const glm::vec3& ray) const {
@@ -22,12 +79,12 @@ namespace vulpes {
             glm::vec3 inv_ray = glm::vec3(1.0f) / ray;
             
             float txmin, txmax, tymin, tymax, tzmin, tzmax;
-            txmin = (Min.x - origin.x) * inv_ray.x;
-            txmax = (Max.x - origin.x) * inv_ray.x;
-            tymin = (Min.y - origin.y) * inv_ray.y;
-            tymax = (Max.y - origin.y) * inv_ray.y;
-            tzmin = (Min.z - origin.z) * inv_ray.z;
-            tzmax = (Max.z - origin.z) * inv_ray.z;
+            txmin = (min.x - origin.x) * inv_ray.x;
+            txmax = (max.x - origin.x) * inv_ray.x;
+            tymin = (min.y - origin.y) * inv_ray.y;
+            tymax = (max.y - origin.y) * inv_ray.y;
+            tzmin = (min.z - origin.z) * inv_ray.z;
+            tzmax = (max.z - origin.z) * inv_ray.z;
 
             float tmin = std::max(std::max(std::min(txmin, txmax), std::min(tymin, tymax)), std::min(tzmin, tzmax));
             float tmax = std::min(std::min(std::max(txmin, txmax), std::max(tymin, tymax)), std::max(tzmin, tzmax));
@@ -42,6 +99,39 @@ namespace vulpes {
 
             return true;
 
+        }
+
+        glm::vec3 AABB::Diagonal() const noexcept {
+            return max - min;
+        }
+
+        float AABB::SurfaceArea() const noexcept {
+            const glm::vec3 diag = Diagonal();
+            return 2.0f * (diag.x * diag.y + diag.x * diag.z + diag.y * diag.z);
+        }
+
+        float AABB::Volume() const noexcept {
+            const glm::vec3 diag = Diagonal();
+            return diag.x * diag.y * diag.z;
+        }
+
+        size_t AABB::IdxToLargestAxis() const noexcept {
+            const glm::vec3 d = Diagonal();
+            if (d.x > d.y && d.x > d.z) {
+                return 0;
+            }
+            else if (d.y > d.z) {
+                return 1;
+            }
+            else {
+                return 2;
+            }
+        }
+
+        glm::vec3 AABB::Lerp(const glm::vec3& p) const noexcept {
+            return glm::vec3(lerp(p.x, min.x, max.x),
+                             lerp(p.y, min.y, max.y),
+                             lerp(p.z, min.z, max.z));
         }
     }
 
