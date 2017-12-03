3ad83df06a82f7cd34e53067e95fd849bafd8769
Keegan <jonesk74@uw.edu>
2017-12-02 20:43:09 -0800

    Make shading data public.

--- include/interaction/SurfaceInteraction.hpp
+++ include/interaction/SurfaceInteraction.hpp
@@ -7,17 +7,19 @@
 namespace vpsk {
 
     class SurfaceInteraction : public Interaction {
-        struct shading_info_t {
-            glm::vec3 n;
-            glm::vec3 dpdu, dpdv;
-            glm::vec3 dndu, dndv;
-        } shadingInfo;
+ 
     public:
 
         SurfaceInteraction(const glm::vec3& p, const glm::vec3& norm, const glm::vec3& p_error,
                            const glm::vec3& neg_ray_dir, const float& time, const glm::vec2& uv,
                            const glm::vec3& dpdu, const glm::vec3& dpdv, const glm::vec3& dndu, const glm::vec3& dndv);
-    
+        
+        struct shading_info_t {
+            glm::vec3 n;
+            glm::vec3 dpdu, dpdv;
+            glm::vec3 dndu, dndv;
+        } shadingInfo;
+
     protected:
         // Coordinates from surface parametrization.
         glm::vec2 uv;
