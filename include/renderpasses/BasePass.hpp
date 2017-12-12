a837fd18c042f1ba4e299cefc5089b52b0099e3e
Keegan <jonesk74@uw.edu>
2017-12-11 16:54:22 -0800

    Continue to implement base renderpass type.

--- include/renderpasses/BasePass.hpp
+++ include/renderpasses/BasePass.hpp
@@ -4,16 +4,20 @@
 #include "ForwardDecl.hpp"
 #include "render/GraphicsPipeline.hpp"
 #include <typeinfo>
+#include <typeindex>
 #include <map>
 
 namespace vpsk {
 
+    using render_function_t = std::function<void(const VkCommandBuffer&)>;
+
     class BasePass {
     public:
-        void RegisterFeature(const std::type_index& idx, const vpr::GraphicsPipelineInfo& info);
+        void RegisterFeature(const std::type_index& idx, const vpr::GraphicsPipelineInfo& info, render_function_t func);
     private:
 
         std::map<std::type_index, vpr::GraphicsPipeline> pipelines;
+        std::map<std::type_index, render_function_t> renderFunctions;
     };
 
 }
--- src/renderpasses/BasePass.cpp
+++ src/renderpasses/BasePass.cpp
@@ -3,6 +3,6 @@ using namespace vpr;
 namespace vpsk {
 
     void BasePass::RegisterFeature(const std::type_index& idx, const GraphicsPipelineInfo& info) {
-        VkGraphicsPipelineCreateInfo info = info.GetCreateInfo();
+        VkGraphicsPipelineCreateInfo info = info.GetPipelineCreateInfo();
     }
 }
