5d21a9240d5f2a52741b72f80e60b427fda49a47
Keegan <jonesk74@uw.edu>
2017-12-29 12:51:25 -0800

    Expand and clean up ShaderGroup a bit.

--- src/render/ShaderGroup.cpp
+++ src/render/ShaderGroup.cpp
@@ -1,18 +1,31 @@
 #include "render/ShaderGroup.hpp"
-
+#include "resource/ShaderModule.hpp"
 namespace vpsk {
+
+    st::ShaderCompiler ShaderGroup::compiler = st::ShaderCompiler();
     
     void ShaderGroup::AddShader(const std::string& raw_path, const VkShaderStageFlags& stage) {
         if(compiler.HasShader(raw_path)) {
             bindings.ParseBinary(compiler.GetBinary(raw_path), stage);
         }
         else {
-            compiler.Compile(raw_path, stage);
-            bindings.ParseBinary(compiler.GetBinary(raw_path), stage);
+            compileAndAddShader(raw_path, stage);
         }
+
+        const auto& binary_code = compiler.GetBinary(raw_path);
+        modules.emplace_back(std::make_unique<vpr::ShaderModule>(dvc, stage, binary_code.data(), static_cast<uint32_t>(binary_code.size())));
+
     }
 
-    const std::vector<uint32_t>& ShaderGroup::CompileAndAddShader(const std::string& raw_path, const VkShaderStageFlags& stage) {
+    std::vector<VkPipelineShaderStageCreateInfo> ShaderGroup::ShaderPipelineInfo() const {
+        std::vector<VkPipelineShaderStageCreateInfo> result;
+        for(const auto& module : modules) {
+            result.push_back(module->PipelineInfo());
+        }
+        return result;
+    }
+
+    const std::vector<uint32_t>& ShaderGroup::compileAndAddShader(const std::string& raw_path, const VkShaderStageFlags& stage) {
         const auto& result = compiler.Compile(raw_path, stage);
         AddShader(raw_path, stage);
         return result;
