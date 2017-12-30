558a1879931428c9b8c8541131b25d3c65c16e82
Keegan <jonesk74@uw.edu>
2017-12-29 16:07:10 -0800

    Add Subpass object
    Single element of greater renderpass. Uses resources owned by renderpass, thus we need to track dependencies and usage conflicts.

---
+++ include/render/Subpass.hpp
@@ -0,0 +1,44 @@
+#pragma once
+#ifndef VPSK_SUBPASS_HPP
+#define VPSK_SUBPASS_HPP
+
+#include "ForwardDecl.hpp"
+#include "RenderResource.hpp"
+#include "util/Delegate.hpp"
+#include <vector>
+
+namespace vpsk {
+
+    class Renderpass;
+
+    class Subpass {
+        Subpass(const Subpass&) = delete;
+        Subpass& operator=(const Subpass&) = delete;
+    public:
+        Subpass(const std::string& name, VkPipelineStageFlags flags);
+
+        void RecordCommands(const VkCommandBuffer& cmd);
+        bool GetAttachmentClearColor(const size_t idx, VkClearColorValue* dest = nullptr);
+        bool GetDepthClearValue(VkClearDepthStencilValue* dest = nullptr);
+        
+        std::string Name;
+        const Renderpass* ParentRenderpass = nullptr;
+        ImageResource* DepthInput = nullptr;
+        ImageResource* DepthOutput = nullptr;
+
+        std::vector<ImageResource*> ColorOutputs;
+        std::vector<ImageResource*> ColorInputs;
+        std::vector<ImageResource*> ResolveOutputs;
+        std::vector<ImageResource*> TextureInputs;
+        std::vector<ImageResource*> AttachmentInputs;
+        std::vector<BufferResource*> UniformInputs;
+
+    private:
+        size_t indexInRenderpass;
+        delegate_t<void(const VkCommandBuffer&)> renderpassCallback;
+        VkPipelineStageFlags stages;
+    };
+
+}
+
+#endif //!VPSK_SUBPASS_HPP
