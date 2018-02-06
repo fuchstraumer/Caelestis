8c0f3369ff671eafff62d5fa247de51787256faf
Keegan <jonesk74@uw.edu>
2018-02-06 00:13:23 -0800

    Working on F+

--- demoscenes/ForwardPlus.cpp
+++ demoscenes/ForwardPlus.cpp
@@ -3,16 +3,31 @@
 #include "core/Instance.hpp"
 #include "resource/DescriptorPool.hpp"
 #include "resource/Allocator.hpp"
+#include "resource/Buffer.hpp"
 #include "util/UtilitySphere.hpp" 
 #include "math/Ray.hpp"
 #include "geometries/Skybox.hpp"
 #include "geometries/ObjModel.hpp"
+#include <memory>
 
 namespace forward_plus {
 
     using namespace vpr;
     using namespace vpsk;
 
+    class LightBuffers {
+    public:
+
+        LightBuffers(const Device* dvc);
+
+        std::unique_ptr<Buffer> Flags;
+        std::unique_ptr<Buffer> Bounds;
+        std::unique_ptr<Buffer> LightCounts;
+        std::unique_ptr<Buffer> LightCountTotal;
+        std::unique_ptr<Buffer> LightCountOffsets;
+        std::unique_ptr<Buffer> LightList;
+        std::unique_ptr<Buffer> LightCountsCompare;
+    };
     
 
 }
