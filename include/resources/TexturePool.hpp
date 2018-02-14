9ea634b644f5f41c52afb88c0c4d72140859d053
Keegan <jonesk74@uw.edu>
2018-02-14 12:12:49 -0800

    Work on a class to pool the backing resources for textures.

---
+++ include/resources/TexturePool.hpp
@@ -0,0 +1,97 @@
+#pragma once
+#ifndef VPSK_TEXTURE_POOL_HPP
+#define VPSK_TEXTURE_POOL_HPP
+#include "ForwardDecl.hpp"
+#include "resource/Texture.hpp"
+#include "tiny_obj_loader.h"
+#include "gli/gli.hpp"
+#include <unordered_map>
+#include <string>
+#include <memory>
+#include <vector>
+namespace vpsk {
+
+    struct misc_material_data_t {
+        float shininess;
+        float ior;
+        float alpha;
+        int illum;
+    };
+
+    struct pbr_material_data_t {
+        float roughness;
+        float metallic;
+        float sheen;
+        float clearcoat_thickness;
+        float clearcoat_roughness;
+        float anisotropy;
+        float anisotropy_rotation;
+        float padding = 0.0f;
+    };
+
+    struct material_ubo_data_t {
+        glm::vec4 ambient;
+        glm::vec4 diffuse;
+        glm::vec4 specular;
+        glm::vec4 transmittance;
+        glm::vec4 emission;
+        misc_material_data_t data;
+        pbr_material_data_t pbrData;
+    };
+
+    using texture_pool_image_entry_t = std::map<std::string, VkDescriptorImageInfo>::const_iterator;
+    using texture_pool_buffer_entry_t = std::map<std::string, VkDescriptorBufferInfo>::const_iterator;
+
+    struct material_pool_entry_t {
+        texture_pool_image_entry_t ambient;
+        texture_pool_image_entry_t diffuse;
+        texture_pool_image_entry_t specular;
+        texture_pool_image_entry_t specularHighlight;
+        texture_pool_image_entry_t bump;
+        texture_pool_image_entry_t displacement;
+        texture_pool_image_entry_t alpha;
+        texture_pool_image_entry_t reflection;
+        texture_pool_image_entry_t roughness;
+        texture_pool_image_entry_t metallic;
+        texture_pool_image_entry_t sheen;
+        texture_pool_image_entry_t emissive;
+        texture_pool_image_entry_t normal;
+        texture_pool_buffer_entry_t buffer;
+    };
+
+    class TexturePool {
+        TexturePool(const TexturePool&) = delete;
+        TexturePool& operator=(const TexturePool&) = delete;
+    public:
+
+        TexturePool(const vpr::Device* dvc, const vpr::TransferPool* transfer_pool);
+        ~TexturePool() = default;
+
+        void AddMaterials(const std::vector<tinyobj::material_t>& material);
+        void AddTexture(const std::string& path);
+        void AddTexture(const std::string& path, const VkFormat format);
+
+        texture_pool_image_entry_t GetTextureDescriptor(const std::string& name);
+        texture_pool_buffer_entry_t GetBufferDescriptor(const std::string& name);
+
+    private:
+
+        std::unordered_map<std::string, VkDescriptorImageInfo> textureDescriptors;
+        std::unordered_map<std::string, VkDescriptorBufferInfo> bufferDescriptors;
+
+        std::unordered_map<std::string, material_ubo_data_t> materialUboData;
+        std::unordered_map<std::string, std::unique_ptr<vpr::Buffer>> materialBuffers;
+        std::unordered_map<std::string, std::unique_ptr<vpr::Texture<vpr::texture_2d_t>>> stbTextures;
+        std::unordered_map<std::string, std::unique_ptr<vpr::Texture<gli::texture2d>>> gli2dTextures;
+        // Uses mtl name to store iterators to all of it's textures.
+        std::unordered_multimap<std::string, decltype(gli2dTextures)::iterator> materialTextures;
+        std::unordered_map<std::string, std::unique_ptr<vpr::Texture<gli::texture_cube>>> gliCubeTextures;
+        std::unordered_map<std::string, std::unique_ptr<vpr::Texture<gli::texture2d_array>>> gliArrayTextures;
+        
+        const vpr::TransferPool* transferPool;
+        const vpr::Device* device;
+    };
+
+}
+
+#endif //!VPSK_TEXTURE_POOL_HPP
---
+++ src/resources/TexturePool.cpp
@@ -0,0 +1,68 @@
+#include "resources/TexturePool.hpp"
+#include "command/TransferPool.hpp"
+namespace vpsk {
+
+    TexturePool::TexturePool(const vpr::Device * dvc, const vpr::TransferPool * transfer_pool) : device(dvc), transferPool(transfer_pool) {}
+
+    void TexturePool::AddMaterials(const std::vector<tinyobj::material_t>& materials) {
+        // Go through the given material and all the texture data to the texture data map.
+        for (const auto& mtl : materials) {
+            auto load_texture_data = [&](const std::string& tex) {
+                if (tex.empty()) {
+                    return;
+                }
+                else {
+                    auto inserted = gli2dTextures.try_emplace(tex, std::make_unique<vpr::Texture<gli::texture2d>>(device));
+                    inserted.first->second->CreateFromFile(tex.c_str());
+                    if (inserted.second) {
+                        // Added a texture, add to material textures.
+                        materialTextures.emplace(mtl.name, inserted.first);
+                    }
+                }
+            };
+
+            auto load_texture_buffer_data = [&]() {
+                auto inserted = materialUboData.try_emplace(mtl.name, material_ubo_data_t{
+                    glm::vec4{ mtl.ambient[0], mtl.ambient[1], mtl.ambient[2], 0.0f },
+                    glm::vec4{ mtl.diffuse[0], mtl.diffuse[1], mtl.diffuse[2], 0.0f },
+                    glm::vec4{ mtl.specular[0], mtl.specular[1], mtl.specular[2], 0.0f },
+                    glm::vec4{ mtl.transmittance[0], mtl.transmittance[1], mtl.transmittance[2], 0.0f },
+                    glm::vec4{ mtl.emission[0], mtl.emission[1], mtl.emission[2], 0.0f },
+                    misc_material_data_t{ mtl.shininess, mtl.ior, mtl.dissolve, mtl.illum },
+                    pbr_material_data_t{ mtl.roughness, mtl.metallic, mtl.sheen, mtl.clearcoat_thickness,
+                        mtl.clearcoat_roughness, mtl.anisotropy, mtl.anisotropy_rotation, 0.0f }
+                });
+
+                if (inserted.second) {
+                    auto inserted = materialBuffers.emplace(mtl.name, std::make_unique<vpr::Buffer>(device));
+                    auto& iter = inserted.first;
+                    iter->second->CreateBuffer(VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT, VK_MEMORY_PROPERTY_HOST_COHERENT_BIT | VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT, sizeof(material_ubo_data_t));
+                    iter->second->CopyToMapped(&materialUboData.at(mtl.name), sizeof(material_ubo_data_t), 0);
+                }
+            }; 
+
+            load_texture_data(mtl.ambient_texname);
+            load_texture_data(mtl.diffuse_texname);
+            load_texture_data(mtl.specular_texname);
+            load_texture_data(mtl.specular_highlight_texname);
+            load_texture_data(mtl.bump_texname);
+            load_texture_data(mtl.displacement_texname);
+            load_texture_data(mtl.alpha_texname);
+            load_texture_data(mtl.reflection_texname);
+            load_texture_data(mtl.roughness_texname);
+            load_texture_data(mtl.metallic_texname);
+            load_texture_data(mtl.sheen_texname);
+            load_texture_data(mtl.normal_texname);
+            load_texture_buffer_data();
+
+            auto bounds = materialTextures.equal_range(mtl.name);
+            auto& cmd = transferPool->Begin();
+            for (auto iter = bounds.first; iter != bounds.second; ++iter) {
+                iter->second->second->TransferToDevice(cmd);
+            }
+            transferPool->Submit();
+
+        }
+    }
+
+}
