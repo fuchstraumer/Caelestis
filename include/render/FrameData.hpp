2f0153c3c8822eea0ec2d3bfda3ded7b8031562d
Keegan <jonesk74@uw.edu>
2018-02-25 09:56:12 -0800

    Begin breaking down clustered forward demo: update shader gen

---
+++ include/render/FrameData.hpp
@@ -0,0 +1,38 @@
+#pragma once
+#ifndef VPSK_FRAME_DATA_HPP
+#define VPSK_FRAME_DATA_HPP
+#include "ForwardDecl.hpp"
+#include <memory>
+#include <vulkan/vulkan.h>
+#include <map>
+#include <string>
+
+namespace vpsk {
+
+    struct cmd_buffer_block_t {
+        VkCommandBuffer Cmd = VK_NULL_HANDLE;
+        VkFence Fence = VK_NULL_HANDLE;
+        bool firstFrame = true;
+    };
+
+    class FrameData {
+        FrameData(const FrameData&) = delete;
+        FrameData& operator=(const FrameData&) = delete;
+    public:
+
+        FrameData(const vpr::Device* dvc);
+        FrameData(FrameData&& other) noexcept;
+        FrameData& operator=(FrameData&& other) noexcept;
+        ~FrameData();
+
+        FrameData& AddCommandBlock(const std::string& name, VkCommandBuffer cmd);
+        cmd_buffer_block_t& GetCommandBlock(const std::string& name);
+
+    private:
+        const vpr::Device* device;
+        std::map<std::string, cmd_buffer_block_t> cmdBlocks;
+    };
+
+}
+
+#endif //!VPSK_FRAME_DATA_HPP
---
+++ src/render/FrameData.cpp
@@ -0,0 +1,10 @@
+#include "render/FrameData.hpp"
+#include "core/LogicalDevice.hpp"
+
+namespace vpsk {
+
+    FrameData::FrameData(const vpr::Device* dvc) : device(dvc) {}
+
+    
+
+}
