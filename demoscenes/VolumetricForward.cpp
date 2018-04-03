5a4795686a54cf712a5f48b0ad2b20e9fac69f3e
Keegan <jonesk74@uw.edu>
2018-04-03 11:12:55 -0700

    Implementing volumetric tiled forward process

--- demoscenes/VolumetricForward.cpp
+++ demoscenes/VolumetricForward.cpp
@@ -20,8 +20,6 @@
 #include "resource/PipelineLayout.hpp"
 #include "resource/DescriptorSetLayout.hpp"
 #include "resource/DescriptorSet.hpp"
-#include "resources/TexturePool.hpp"
-#include "geometries/ObjModel.hpp"
 #include "scene/Window.hpp"
 #include "resources/Multisampling.hpp"
 #include "render/DepthStencil.hpp"
@@ -29,21 +27,14 @@
 #include "resources/LightBuffers.hpp"
 #include "render/FrameData.hpp"
 #include "render/BackBuffer.hpp"
-#include "resources/ShaderGroup.hpp"
 #include <memory>
 #include <map>
 #include <random>
 #include <deque>
 #include <experimental/filesystem>
 
-#include "ShaderGenerator.hpp"
-#include "Compiler.hpp"
-#include "BindingGenerator.hpp"
-#include "mango/mango/mango.hpp"
-
 #include "util/easylogging++.h"
 INITIALIZE_EASYLOGGINGPP
-
 namespace volumetric_forward {
 
     constexpr uint32_t LightGridBlockSize = 32;
@@ -64,14 +55,43 @@ namespace volumetric_forward {
         std::unique_ptr<vpr::DescriptorSet> Descriptor;
     };
 
+    /*
+        U_IMAGE_BUFFER r32ui PointLightIndexCounter;
+        U_IMAGE_BUFFER r32ui SpotLightIndexCounter;
+        U_IMAGE_BUFFER r32ui PointLightIndexList;
+        U_IMAGE_BUFFER r32ui SpotLightIndexList;
+        U_IMAGE_BUFFER rg32ui PointLightGrid;
+        U_IMAGE_BUFFER rg32ui SpotLightGrid;
+        U_IMAGE_BUFFER r32ui InputKeys;
+        U_IMAGE_BUFFER r32ui InputValues;
+        U_IMAGE_BUFFER r32ui OutputKeys;
+        U_IMAGE_BUFFER r32ui OutputValues;
+        I_IMAGE_BUFFER r32i MergePathPartitions;
+    */
+
+    resources_collection_t MergeSortResources { 
+        std::unordered_map<std::string, std::unique_ptr<vpr::Buffer>> {
+            { "SortParams", nullptr },
+            { "InputKeys", nullptr },
+            { "InputValues", nullptr },
+            { "OutputKeys", nullptr },
+            { "OutputValues", nullptr },
+            { "MergePathPartitions", nullptr }
+        },
+        std::unique_ptr<vpr::DescriptorSet>{ nullptr }
+    };
+
     struct compute_pipeline_t {
+        compute_pipeline_t() = default;
+        ~compute_pipeline_t() = default;
         VkPipeline Handle{ VK_NULL_HANDLE };
         VkComputePipelineCreateInfo Info{ vpr::vk_compute_pipeline_create_info_base };
+        std::unique_ptr<vpr::PipelineLayout> Layout{ nullptr };
         std::unique_ptr<vpr::PipelineCache> Cache{ nullptr };
     };
 
     struct compute_pipelines_t {
-        std::unordered_map<std::string, compute_pipeline_t> Handles{
+        std::unordered_map<std::string, compute_pipeline_t> Handles = std::unordered_map<std::string, compute_pipeline_t>{
             { "UpdateLights", compute_pipeline_t() },
             { "ReduceLights1", compute_pipeline_t() },
             { "ReduceLights2", compute_pipeline_t() },
@@ -86,7 +106,49 @@ namespace volumetric_forward {
             { "IndirectArgsUpdate", compute_pipeline_t() },
             { "AssignLightsToClusters", compute_pipeline_t() }
         };
+
+        compute_pipeline_t& at(const std::string& key);
     } ComputePipelines;
+
+    struct alignas(4) SortParams {
+        uint32_t NumElements;
+        uint32_t ChunkSize;
+    };
     
-    
+
+
+    constexpr static uint32_t SORTING_NUM_THREADS_PER_THREAD_GROUP = 256;
+    constexpr static uint32_t SORT_NUM_VALUES_PER_THREAD = 8;
+
+    void MergeSort(const VkCommandBuffer& cmd, vpr::Buffer* src_keys, vpr::Buffer* src_values, vpr::Buffer* dst_keys, 
+        vpr::Buffer* dst_values, const uint32_t num_values, const uint32_t chunk_size) {
+        
+        auto& merge_path_partitions = MergeSortResources.Resources.at("MergePathPartitions");
+        auto& merge_pipeline = ComputePipelines.at("MergePathPartitions");
+
+        constexpr uint32_t numThreadsPerGroup = SORTING_NUM_THREADS_PER_THREAD_GROUP;
+        constexpr uint32_t numValuesPerThread = SORT_NUM_VALUES_PER_THREAD;
+        constexpr uint32_t numValuesPerThreadGroup = numThreadsPerGroup * numValuesPerThread;
+
+        uint32_t num_chunks = static_cast<uint32_t>(std::ceil(static_cast<double>(num_values) / static_cast<double>(chunk_size)));
+        uint32_t pass = 0;
+        uint32_t chunk_size_local = chunk_size;
+
+        while (num_chunks > 1) {
+
+            const SortParams params{ num_values, chunk_size_local };
+            const uint32_t num_sort_groups = num_chunks / 2;
+            const uint32_t num_thread_groups_per_sort_group = static_cast<uint32_t>(
+                std::ceil(static_cast<double>(chunk_size * 2) / static_cast<double>(numValuesPerThreadGroup))
+            );
+
+            {
+                merge_path_partitions->Fill(cmd, merge_path_partitions->Size(), 0, 0);
+                vkCmdBindPipeline(cmd, VK_PIPELINE_BIND_POINT_COMPUTE, ComputePipelines.at("MergePathPartitions").Handle);
+                vkCmdBindDescriptorSets(cmd, VK_PIPELINE_BIND_POINT_COMPUTE, merge_pipeline.Layout->vkHandle(),
+                    0, 1, &MergeSortResources.Descriptor->vkHandle(), 0, nullptr);
+            }
+
+        }
+    }
 }
