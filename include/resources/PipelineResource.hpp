90c1ff3fcdb460f02968ed9bd763bb2db89368e4
Keegan <jonesk74@uw.edu>
2018-04-06 17:01:12 -0700

    Finishing up current implementation of pipeline resource

--- include/resources/PipelineResource.hpp
+++ include/resources/PipelineResource.hpp
@@ -9,13 +9,13 @@
 
 namespace vpsk {
     
-    struct BufferInfo {
+    struct buffer_info_t {
         VkDeviceSize Size{ 0 };
-        VkBufferUsageFlags Usage{ 0 };
-        VkDeviceSize ViewOffset{ 0 };
+        VkBufferUsageFlags Usage{ VK_BUFFER_USAGE_FLAG_BITS_MAX_ENUM };
+        bool operator==(const buffer_info_t& other) const noexcept;
     };
 
-    struct ImageInfo {
+    struct image_info_t {
         enum class size_class {
             Absolute,
             SwapchainRelative,
@@ -28,32 +28,74 @@ namespace vpsk {
         uint32_t Layers{ 1 };
         float Anisotropy{ 1.0f };
         std::string SizeRelativeName{};
-        VkImageUsageFlags Usage{ 0 };
+        VkImageUsageFlags Usage{ VK_IMAGE_USAGE_FLAG_BITS_MAX_ENUM };
+        VkFormat Format{ VK_FORMAT_UNDEFINED };
+        bool operator==(const image_info_t& other) const noexcept;
+    };
+
+    using resource_info_variant_t = std::variant<
+        buffer_info_t,
+        image_info_t
+    >;
+
+    struct ResourceDimensions {
+        VkFormat Format{ VK_FORMAT_UNDEFINED };
+        buffer_info_t BufferInfo;
+        uint32_t Width{ 0 };
+        uint32_t Height{ 0 };
+        uint32_t Depth{ 1 };
+        uint32_t Layers{ 1 };
+        uint32_t Levels{ 1 };
+        uint32_t Samples{ 1 };
+        bool Transient{ false };
+        bool Persistent{ true };
+        bool Storage{ false };
+        VkPipelineStageFlags UsedStages{ VK_PIPELINE_STAGE_FLAG_BITS_MAX_ENUM };
     };
 
     class PipelineResource {
     public:
 
-        PipelineResource(std::string name, const std::variant<BufferInfo, ImageInfo>& _info);
+        PipelineResource(std::string name, size_t idx);
+
+        bool operator==(const PipelineResource& other) const noexcept;
+
+        bool IsBuffer() const noexcept;
+        bool IsImage() const noexcept;
+
+        void WrittenInPass(uint16_t idx);
+        void ReadInPass(uint16_t idx);
+
+        void SetIdx(size_t idx);
+        void SetParentSetIdx(size_t idx);
+        void SetName(std::string _name);
+        void SetDescriptorType(VkDescriptorType type);
+        void AddUsedPipelineStages(VkPipelineStageFlags stages);
+        void SetInfo(resource_info_variant_t _info);
+
+        const size_t& GetIdx() const noexcept;
+        const size_t& GetParentSetIdx() const noexcept;
+        const VkDescriptorType& GetDescriptorType() const noexcept;
+        const std::string& GetName() const noexcept;
+        VkPipelineStageFlags GetUsedPipelineStages() const noexcept;
+        const std::unordered_set<uint16_t>& GetPassesReadIn() const noexcept;
+        const std::unordered_set<uint16_t>& GetPassesWrittenIn() const noexcept;
+        const resource_info_variant_t& GetInfo() const noexcept;
 
     private:
 
+        size_t idx;
+        // We don't care about the unique binding index of a resource relative to a set: just what
+        // set it belongs to, as that helps us group things in a more important manner
+        size_t parentSetIdx{ std::numeric_limits<size_t>::max() };
         // Usually the name read from the shader
         std::string name{ };
         // Extracted from the shader
         VkDescriptorType intendedType{ VK_DESCRIPTOR_TYPE_MAX_ENUM };
-        // Says we don't actually need backing memory
-        bool transient{ false };
-        // Persists beyond the lifetime of the renderpass
-        bool persistent{ true };
-        // Used as storage, i.e a generalized format thats more compatible 
-        bool storage{ false };
         // Stages of the pipeline this item is used in
-        VkPipelineStageFlags stages{ 0 };
+        VkPipelineStageFlags usedStages{ VK_PIPELINE_STAGE_FLAG_BITS_MAX_ENUM };
 
-        std::variant<BufferInfo, ImageInfo> info;
-        vpr::Buffer* backingBuffer{ nullptr };
-        vpr::Image* backingImage{ nullptr };
+        resource_info_variant_t info;
 
         std::unordered_set<uint16_t> readInPasses;
         std::unordered_set<uint16_t> writtenInPasses;
--- src/resources/PipelineResource.cpp
+++ src/resources/PipelineResource.cpp
@@ -1,4 +1,71 @@
 #include "resources/PipelineResource.hpp"
 namespace vpsk {
 
+
+    PipelineResource::PipelineResource(std::string _name, resource_info_variant_t _info) : name(std::move(_name)),
+        info(std::move(_info)) {}
+
+    bool PipelineResource::IsBuffer() const noexcept {
+        return std::holds_alternative<buffer_info_t>(info);
+    }
+
+    bool PipelineResource::IsImage() const noexcept {
+        return std::holds_alternative<image_info_t>(info);
+    }
+
+    void PipelineResource::WrittenInPass(uint16_t idx) {
+        writtenInPasses.emplace(std::move(idx));
+    }
+
+    void PipelineResource::ReadInPass(uint16_t idx) {
+        readInPasses.emplace(std::move(idx));
+    }
+
+    void PipelineResource::SetIdx(size_t _idx) {
+        idx = std::move(_idx);
+    }
+
+    void PipelineResource::SetParentSetIdx(size_t idx) {
+        parentSetIdx = std::move(idx);
+    }
+
+    void PipelineResource::SetName(std::string _name) {
+        name = std::move(_name);
+    }
+
+    void PipelineResource::SetDescriptorType(VkDescriptorType type) {
+        intendedType = std::move(type);
+    }
+
+    void PipelineResource::AddUsedPipelineStages(VkPipelineStageFlags stages) {
+        usedStages |= stages;
+    }
+
+    const size_t& PipelineResource::GetIdx() const noexcept {
+        return idx;
+    }
+
+    const size_t& PipelineResource::GetParentSetIdx() const noexcept {
+        return parentSetIdx;
+    }
+
+    const VkDescriptorType& PipelineResource::GetDescriptorType() const noexcept {
+        return intendedType;
+    }
+
+    const std::string& PipelineResource::GetName() const noexcept {
+        return name;
+    }
+
+    VkPipelineStageFlags PipelineResource::GetUsedPipelineStages() const noexcept {
+        return usedStages;
+    }
+
+    const std::unordered_set<uint16_t>& PipelineResource::GetPassesReadIn() const noexcept {
+        return readInPasses;
+    }
+
+    const std::unordered_set<uint16_t>& PipelineResource::GetPassesWrittenIn() const noexcept {
+        return writtenInPasses;
+    }
 }
