a0d09568d7ccbfc584e2ed20b45a9b26d0164b81
Keegan <jonesk74@uw.edu>
2018-05-04 11:57:44 -0700

    Redefine entity class by removing it
    Entity is now literally just a 32 bit integer. Important traits about this are stored in a static entity_traits class, based on the type of the entity.. the integer. Used entt's family registration system as well.

--- include/core/Entity.hpp
+++ include/core/Entity.hpp
@@ -1,14 +1,49 @@
 #pragma once
 #ifndef VPSK_ENTITY_HPP
 #define VPSK_ENTITY_HPP
-#include "Event.hpp"
-#include <memory>
+#include <cstdint>
 #include <type_traits>
-#include <bitset>
-#include <tuple>
+#include <atomic>
 
 namespace vpsk {
 
+    template<typename>
+    struct EntityTraits;
+
+    template<>
+    struct EntityTraits<std::uint32_t> {
+        using entity_type = std::uint32_t;
+        using version_type = std::uint16_t;
+        // Extract lower 24 bits representing the entity number / UUID
+        static constexpr auto entity_mask = 0xFFFFF;
+        // Extract upper 8 bits for the version
+        static constexpr auto version_mask = 0xFFF;
+        static constexpr auto entity_shift = 20;
+    };
+
+    template<typename...>
+    class Family {
+        static std::atomic<std::size_t> id;
+
+        template<typename...>
+        static std::size_t getID() noexcept {
+            static const std::size_t value = id.fetch_add(1);
+            return value;
+        }
+
+    public:
+        using family_type = std::size_t;
+
+        // Used to return a unique identifier for the given type
+        template<typename...Ts>
+        inline static family_type Type() noexcept {
+            return getID<std::decay_t<Ts>...>();
+        }
+
+    };
+
+    template<typename...Ts>
+    std::atomic<std::size_t> Family<Ts...>::id{};
 
 }
 
