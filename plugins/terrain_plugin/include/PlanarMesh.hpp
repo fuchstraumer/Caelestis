21608f95d9cb8e97aee4b899e3f3550249713476
Keegan <jonesk74@uw.edu>
2018-07-24 17:58:13 -0700

    Clean up and formatting Pt2

--- plugins/terrain_plugin/include/PlanarMesh.hpp
+++ plugins/terrain_plugin/include/PlanarMesh.hpp
@@ -1,6 +1,6 @@
 #pragma once
-#ifndef VULPES_PLANAR_MESH_H
-#define VULPES_PLANAR_MESH_H
+#ifndef TERRAIN_PLUGIN_PLANAR_MESH_HPP
+#define TERRAIN_PLUGIN_PLANAR_MESH_HPP
 
 #include "stdafx.h"
 #include "mesh.hpp"
@@ -11,30 +11,15 @@ namespace vulpes {
 	}
 }
 
-namespace vulpes {
+class PlanarMesh : public Mesh<mesh::Vertices, vertex_t> {
+public:
+    PlanarMesh(const double& side_length, const glm::ivec3& grid_pos, const glm::vec3& pos = glm::vec3(0.0f), const glm::vec3& scale = glm::vec3(1.0f), const glm::vec3& angle = glm::vec3(0.0f));
+    PlanarMesh() ;
+    ~PlanarMesh();
+    size_t SubdivisionLevel;
+    double SideLength;
+    glm::ivec3 GridPos;
+    void Generate(terrain::HeightNode* height_nodes);
+};
 
-	namespace mesh {
-
-
-
-		class PlanarMesh : public Mesh<mesh::Vertices, vertex_t> {
-		public:
-
-			PlanarMesh(const double& side_length, const glm::ivec3& grid_pos, const glm::vec3& pos = glm::vec3(0.0f), const glm::vec3& scale = glm::vec3(1.0f), const glm::vec3& angle = glm::vec3(0.0f));
-
-			PlanarMesh& operator=(PlanarMesh&& other);
-
-			PlanarMesh() = default;
-			virtual ~PlanarMesh() = default;
-
-			size_t SubdivisionLevel;
-			double SideLength;
-			glm::ivec3 GridPos;
-			void Generate(terrain::HeightNode* height_nodes);
-
-		};
-
-	}
-}
-
-#endif // !VULPES_PLANAR_MESH_H
+#endif // !TERRAIN_PLUGIN_PLANAR_MESH_HPP
