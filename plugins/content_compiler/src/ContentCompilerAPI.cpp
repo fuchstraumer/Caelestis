a0a486f6c9618e69b0061bd1a78d861de2424b0f
Keegan <jonesk74@uw.edu>
2018-09-16 16:02:42 -0700

    Fix content compiler.

--- plugins/content_compiler/src/ContentCompilerAPI.cpp
+++ plugins/content_compiler/src/ContentCompilerAPI.cpp
@@ -14,8 +14,14 @@ static ResourceContext_API* resource_api = nullptr;
 static ApplicationContext_API* application_api = nullptr;
 static std::vector<std::string> loadedFiles;
 
-static void* LoadMeshData(const char* fname) {
-    return AssimpLoadMeshData(fname, true);
+static MeshProcessingOptions opts{
+    true,
+    false,
+    true
+};
+
+static void* LoadMeshData_void(const char* fname, void* user_data) {
+    return AssimpLoadMeshData(fname, reinterpret_cast<MeshProcessingOptions*>(user_data));
 }
 
 static void DestroyMeshData_void(void* ptr) {
@@ -40,7 +46,7 @@ static void Load(GetEngineAPI_Fn fn) {
     application_api = reinterpret_cast<ApplicationContext_API*>(fn(APPLICATION_CONTEXT_API_ID));
     el::Helpers::setStorage(*reinterpret_cast<el::base::type::StoragePointer*>(application_api->GetLoggingStoragePointer()));
     WriteLoadedLog();
-    resource_api->RegisterFileTypeFactory("ASSET_PIPELINE_MESH", LoadMeshData, DestroyMeshData_void);
+    resource_api->RegisterFileTypeFactory("ASSET_PIPELINE_MESH", LoadMeshData_void, DestroyMeshData_void);
 }
 
 static void Unload() {
@@ -57,13 +63,9 @@ static void TimeDependentUpdate(double dt) {
 
 }
 
-MeshData* LoadMeshData(const char* fname, bool interleaved) {
-    return AssimpLoadMeshData(fname, interleaved);
-}
-
-void LoadMeshDataAsync(const char* fname, bool interleaved, void* requester, ContentCompiler_API::mesh_loaded_signal_t signal) {
+void LoadMeshDataAsync(const char* fname, bool interleaved, void* requester, ContentCompiler_API::mesh_loaded_signal_t signal, MeshProcessingOptions* options) {
     loadedFiles.emplace_back(std::string(fname));
-    resource_api->LoadFile("ASSET_PIPELINE_MESH", fname, requester, signal, nullptr);
+    resource_api->LoadFile("ASSET_PIPELINE_MESH", fname, requester, signal, options);
 }
 
 static Plugin_API* GetCoreAPI() {
@@ -79,7 +81,7 @@ static Plugin_API* GetCoreAPI() {
 
 static ContentCompiler_API* GetAssetAPI() {
     static ContentCompiler_API api{ nullptr };
-    api.LoadMeshFromFileAssimp = LoadMeshData;
+    api.LoadMeshFromFileAssimp = AssimpLoadMeshData;
     api.AsyncLoadMeshFromFileAssimp = LoadMeshDataAsync;
     api.DestroyMeshData = MeshData::DestroyMeshData;
     return &api;
