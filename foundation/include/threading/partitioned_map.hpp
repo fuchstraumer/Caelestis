8b9e67b6e8715c2d38f3350a14afa6117a591ac3
Keegan <jonesk74@uw.edu>
2019-04-01 21:18:04 -0700

    Fixing CMake errors

---
+++ foundation/include/threading/partitioned_map.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#ifndef FOUNDATION_PARTITIONED_MAP_HPP
+#define FOUNDATION_PARTITIONED_MAP_HPP
+
+namespace foundation
+{
+
+    namespace detail
+    {
+        
+    }
+
+    template<typename KeyType, typename ValueType>
+    class partitioned_threadsafe_map
+    {
+
+    };
+
+}
+
+#endif // !FOUNDATION_PARTITIONED_MAP_HPP
